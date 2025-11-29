9adeacb4688b250923327d53761a675865bc7581
nolan-middleton <nolan.middleton@wsu.edu>
2025-11-28 16:57:07 -0800

    Ran the kNearestNeighbors and DecisionTree

--- RandomForest.py
+++ RandomForest.py
@@ -5,7 +5,7 @@ import Functions as F
 
 # Parameters
 depths = [2,3,4] # The max_depths to test
-n_estimators = [100] + [1000 * n for n in range(1,5)] # n_estimators to test
+n_estimators = [100, 200, 500, 1000, 2000] # n_estimators to test
 
 # Defs
 def model_function(training_X, training_Y):
@@ -14,8 +14,8 @@ def model_function(training_X, training_Y):
         print("=> Depth: " + str(depth) + "...")
         results[str(depth)] = {}
         for n_estimator in n_estimators:
-            print("==> Number of Estimators: " + str(n_estimators) + "...")
-            results[str(depth)][str(n_estimators)]=F.leave_one_out_validation(
+            print("==> Number of Estimators: " + str(n_estimator) + "...")
+            results[str(depth)][str(n_estimator)]=F.leave_one_out_validation(
                 F.train_random_forest,
                 training_X,
                 training_Y,
